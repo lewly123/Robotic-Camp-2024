f7e3506b6a535d09e40b163855dd4f7ace4a07a9
lewly123AsusV1 <idkwhoami9999@gmail.com>
2024-12-16 22:00:34 +0800

    remove debug consolelog

--- database/logout.js
+++ database/logout.js
@@ -1,14 +1,12 @@
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 import { updateUI } from "./function.js";
 
-console.log("2");
 // Get the logout button element
 document.addEventListener("DOMContentLoaded", () => {
     const auth = getAuth();
     const logoutButton = document.getElementById("logoutButton");
 
     if (logoutButton) {
-        console.log("3");
         logoutButton.addEventListener("click", function (e) {
             e.preventDefault(); // Prevent default anchor behavior
             console.log("1");
